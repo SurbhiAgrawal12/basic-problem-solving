72a955f43111e3b2683f195c7afbc9d10f0c39fa
SurbhiAgrawal12 <60575705+SurbhiAgrawal12@users.noreply.github.com>
2020-02-18 07:32:40 +0530

    Push Zeros To End

--- Array/pushZerosToEnd.js
+++ Array/pushZerosToEnd.js
@@ -11,3 +11,33 @@ remain the same.
 2) Both pointers will be initialised with 0.
 3) 
 */
+function swap(arr, currentPointer, zeroPointer) {
+    let temp = arr[currentPointer];
+    arr[currentPointer] = arr[zeroPointer];
+    arr[zeroPointer] = temp;
+}
+
+function pushZerosToEnd(arr) {
+    let zeroPointer = arr.length-1;
+    let currentPointer =0;
+
+    for(;currentPointer < zeroPointer; ){
+        if(arr[currentPointer] == 0 && arr[zeroPointer] != 0){
+            swap(arr, currentPointer, zeroPointer);
+            currentPointer++; zeroPointer--
+
+        }
+        else if(arr[currentPointer] == 0 && arr[zeroPointer] == 0){
+            zeroPointer--;
+        }
+        else if(arr[currentPointer] != 0){
+            currentPointer++;
+        }
+    }
+}
+
+
+let arr = [2, 0, 4, 1, 3, 0, 0];
+console.log("original arr :: ", arr);
+pushZerosToEnd(arr);
+console.log("final array ::",arr);

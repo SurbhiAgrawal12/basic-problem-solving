ab19b49e996b9ed2e267f8a6ea77ee5320ed343c
SurbhiAgrawal12 <60575705+SurbhiAgrawal12@users.noreply.github.com>
2020-02-20 23:00:08 +0530

    Binary Search

--- SearchingAlgorithm/BinarySearch.js
+++ SearchingAlgorithm/BinarySearch.js
@@ -11,3 +11,30 @@ the array, return -1.
 2. If middle element is less than x, we’ll do the search in the right half of the array.
 3. If middle element is more than x, we’ll do the search in the left half of the array.
 */
+
+function binarySearch(arr, elementToSearch) {
+    let startIndex = 0;
+    let endIndex = arr.length-1;
+    
+    while(startIndex <= endIndex) {
+
+        let midIndex = Math.trunc((endIndex + startIndex)/2); // toFixed converts the num into string
+        
+        if(arr[midIndex] == elementToSearch) {
+            return midIndex;
+        }
+        else if(arr[midIndex] < elementToSearch) {
+            startIndex =  midIndex + 1;    
+        }else{
+            endIndex = midIndex-1;
+        } 
+    }
+    
+    return -1;
+}
+
+
+let arr = [1, 3, 7, 9, 11, 12, 45];
+let elementToSearch = 0;
+console.log(binarySearch(arr, elementToSearch));
+

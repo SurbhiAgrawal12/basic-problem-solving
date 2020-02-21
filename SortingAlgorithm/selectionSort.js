26b7a71db41fb99490fa6cd7d5402f28ad395da1
SurbhiAgrawal12 <60575705+SurbhiAgrawal12@users.noreply.github.com>
2020-02-21 10:58:07 +0530

    Sort the array using Selection Sort

--- SortingAlgorithm/selectionSort.js
+++ SortingAlgorithm/selectionSort.js
@@ -9,3 +9,28 @@ Change in the input array itself. You don't need to return or print elements.
 The selection sort algorithm sorts an array by repeatedly finding the 
 minimum element (considering ascending order) from unsorted part and putting it at the beginning. 
 */
+
+function swap(arr, j, min, minIndex) {
+    let temp = arr[j];
+    arr[j] = min;
+    arr[minIndex] = temp;
+}
+
+function selectionSort(arr) {
+
+    let [min] = arr;
+    for(let j=0; j<arr.length; j++){
+        let min = arr[j];
+        for(let i =j+1; i< arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+        swap(arr, j, min, minIndex)   
+    }
+}
+
+let arr = [5,1,6,7,9,2,4,5,7];
+selectionSort(arr);
+console.log("arr :: ", arr);
